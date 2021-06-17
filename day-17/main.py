9de1f1a7e36739e8b96dbac91083c58aaf8cc9c4
christina <chrstnaheadley@gmail.com>
2021-06-17 14:03:08 -0400

    adds day 17

---
+++ day-17/main.py
@@ -0,0 +1,9 @@
+class User:
+    def __init__(self):
+
+user_1 = User()
+user_1.id = 1
+
+
+def function():
+    pass
---
+++ turtle-projects/main.py
@@ -0,0 +1,36 @@
+import turtle
+from turtle import Turtle, Screen
+
+timmy_the_turtle = Turtle()
+timmy_the_turtle.shape("turtle")
+
+# #make a square
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(100)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
