c5fe8f1a345029a40f9be364c60f300eff9ccb0f
christina <chrstnaheadley@gmail.com>
2021-06-29 11:25:07 -0400

    adds day 24 mail merge

--- Mail-Merge-Project-Start/main.py
+++ Mail-Merge-Project-Start/main.py
@@ -7,14 +7,11 @@
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
 
-
-# with open("../Mail-Merge-Project-Start/Input/Letters.starting_letter.txt") as file:
-# with open("../Mail-Merge-Project-Start/Input/Letters/starting_letter.txt") as file:
-with open("../Mail-Merge-Project-Start/Input/Names/invited_names.txt") as file:
-    names = file.readlines()
-print(names)
-# with open("/..")
-# for name in names:
-#     txt.replace("[name]", name)
-# .strip
-# ("/python/ang...")``
+with open("./Input/Names/invited_names.txt") as names_file:
+    names = names_file.readlines()
+with open("./Input/Letters/starting_letter.txt") as letter_file:
+    letter = letter_file.read()
+    for name in names:
+      new_letter = letter.replace("[name]", name.strip())
+      with open(f"letter_for_{name}", mode="w") as final_letter:
+        final_letter.write(new_letter)
